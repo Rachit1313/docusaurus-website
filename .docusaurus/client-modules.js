48e54bf9ca4be955acb88485f9ccd3f23e57de78
Rachit1313 <Rachitchawla33@gmail.com>
2023-10-31 10:05:55 -0400

    Added readme

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/rachit/Downloads/Seneca/CPAsem5/OSD600/docusaurus-website/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/rachit/Downloads/Seneca/CPAsem5/OSD600/docusaurus-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/rachit/Downloads/Seneca/CPAsem5/OSD600/docusaurus-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/rachit/Downloads/Seneca/CPAsem5/OSD600/docusaurus-website/src/css/custom.css'),
+];
